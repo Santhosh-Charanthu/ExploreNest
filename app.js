07c9eb8b301e62237aea3bb70f51bad8f1ec21ed
Santhosh-Charanthu <santhoshcharanthu455@gmail.com>
2025-05-23 17:36:13 +0530

    ExploreNest

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV != "production") {
   require("dotenv").config();
 }
 
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+
+dbUrl = process.env.ATLASDB_URL;
+
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
@@ -16,54 +19,43 @@ const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
-const cors = require("cors");
-const helmet = require("helmet");
-const mongoSanitize = require("express-mongo-sanitize");
 
-// Routes
 const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
+const { log } = require("console");
 
-// MongoDB URL
-const dbUrl = process.env.ATLASDB_URL;
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
+app.use(cookieParser("secretcode"));
 
-// Connect to MongoDB
 main()
-  .then(() => console.log("Connected to DB"))
-  .catch((err) => console.log(err));
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 async function main() {
   await mongoose.connect(dbUrl);
 }
 
-// Set view engine and middleware
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-app.engine("ejs", ejsMate);
-app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
-app.use(express.static(path.join(__dirname, "/public")));
-app.use(cookieParser("secretcode"));
-
-// Security middleware
-app.use(helmet());
-app.use(mongoSanitize());
-app.use(
-  cors({
-    origin: "https://your-frontend-url.com", // Replace with your frontend
-    credentials: true,
-  })
-);
-
-// Session store
 const store = MongoStore.create({
   mongoUrl: dbUrl,
-  crypto: { secret: process.env.SECRET },
+  crypto: {
+    secret: process.env.SECRET,
+  },
   touchAfter: 24 * 3600,
 });
 
-store.on("error", (err) => console.log("SESSION STORE ERROR", err));
+store.on("error", (err) => {
+  console.log("ERROR in MONGO SESSION STORE", err);
+});
 
 const sessionOptions = {
   store,
@@ -80,14 +72,14 @@ const sessionOptions = {
 app.use(session(sessionOptions));
 app.use(flash());
 
-// Passport config
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
+
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-// Flash messages and user data
+//Middleware
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
@@ -96,23 +88,20 @@ app.use((req, res, next) => {
   next();
 });
 
-// Routes
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
 
-// 404 Error handler
 app.all("*", (req, res, next) => {
   next(new ExpressError(404, "Page not found!"));
 });
 
-// Global Error handler
 app.use((err, req, res, next) => {
-  const { statusCode = 500, message = "Something went wrong" } = err;
-  res.status(statusCode).render("error.ejs", { message });
+  let { statusCode = 500, message = "Something went wrong" } = err;
+  res.render("error.ejs", { message });
+  // res.status(statusCode).send(message);
 });
 
-// Start server
 app.listen(7070, () => {
-  console.log("Server is listening on port 7070");
+  console.log("server is listening to port 7070");
 });
